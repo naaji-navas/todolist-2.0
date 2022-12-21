058ae56f5e8b02cb569a805ea424a92463ad16f2
naaji-navas <najidnavas2000@gamil.com>
2022-12-21 17:39:55 +0530

    chore: able to add new item

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.get("/", function (req, res) {
         if (err) console.log(err);
         else console.log("Successfully saved default items to DB.");
       });
+      res.redirect("/");
     } else {
       res.render("list", { listTitle: "Today", newListItems: foundItems });
     }
@@ -60,16 +61,16 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+  
 });
+
 
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
