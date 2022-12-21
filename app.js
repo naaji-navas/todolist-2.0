292c50e30d993814ba63b0db53053bb742a34f3b
naaji-navas <najidnavas2000@gamil.com>
2022-12-21 21:57:05 +0530

    connected to mongodb atlas

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://naji_navas1:gpDvv2eu1k4TbzuX@cluster0.1xlx8kd.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //Schema
 const itemsSchema = {
