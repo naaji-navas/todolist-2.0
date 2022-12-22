1374c4cbdb3807e5d43e47b6285b183104d73fe0
naaji-navas <najidnavas2000@gamil.com>
2022-12-22 15:43:59 +0530

    added port for deployment

--- app.js
+++ app.js
@@ -156,6 +156,10 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
+
+// app.listen(3000, function () {
+//   console.log("Server started on port 3000");
+// });
